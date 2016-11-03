0aefa21bba91d9da1588df01d91a3b245d87ceaf
lagoyu <paul@shaddick.net>
2016-11-03 12:19:13 +0000

    comments

--- Program.cs
+++ Program.cs
@@ -10,11 +10,16 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            // Use constants instead of 'magic numbers'
             const int fizzFactor = 3;
-            const int buzzFactor = 7;
+            const int buzzFactor = 5;
 
             for (int i = 1; i <= 100; i++)
             {
+                // % is the remainder operator
+                // See https://msdn.microsoft.com/en-us/library/0w4e0fzs.aspx
+                // && is logical AND operator
+                // See https://www.tutorialspoint.com/csharp/csharp_logical_operators.htm
                 if ((i % fizzFactor == 0) && (i % buzzFactor == 0))
                 {
                     Console.WriteLine("FizzBuzz");
