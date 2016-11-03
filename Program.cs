1d1a591b40001c5d035adccac62bf2b0c0eb6be8
lagoyu <paul@shaddick.net>
2016-11-03 12:07:53 +0000

    replaced magic numbers with constants

--- Program.cs
+++ Program.cs
@@ -10,17 +10,20 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            const int fizzFactor = 3;
+            const int buzzFactor = 7;
+
             for (int i = 1; i <= 100; i++)
             {
-                if ((i % 3 == 0) && (i % 5 == 0))
+                if ((i % fizzFactor == 0) && (i % buzzFactor == 0))
                 {
                     Console.WriteLine("FizzBuzz");
                 }
-                else if (i % 3 == 0)
+                else if (i % fizzFactor == 0)
                 {
                     Console.WriteLine("Fizz");
                 }
-                else if (i % 5 == 0)
+                else if (i % buzzFactor == 0)
                 {
                     Console.WriteLine("Buzz");
                 }
